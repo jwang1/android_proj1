f9a2520261dc166ab49ce61437bdabb546426d4b
jwang1 <sigkdd@yahoo.com>
2016-12-04 01:11:21 -0500

    Improvement: moved Location setting to its own Header (out of the General Header), to find it easier.
    TODO:  Define an Intent to go back DIRECTLY to "MainActivity", after clicking "back arrow" on menu,
    to enhance user-experience.

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -173,7 +173,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     return PreferenceFragment.class.getName().equals(fragmentName)
         || GeneralPreferenceFragment.class.getName().equals(fragmentName)
         || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
-        || NotificationPreferenceFragment.class.getName().equals(fragmentName);
+        || NotificationPreferenceFragment.class.getName().equals(fragmentName)
+        || LocationPreferenceFragment.class.getName().equals(fragmentName);
   }
 
   /**
@@ -198,7 +199,30 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
       bindPreferenceSummaryToValue(findPreference("example_text"));
       bindPreferenceSummaryToValue(findPreference("example_list"));
 
-      // JWANG: for the location binding
+      // Moved to newly created LoctionPreference
+      // // JWANG: for the location binding
+      //bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      int id = item.getItemId();
+      if (id == android.R.id.home) {
+        startActivity(new Intent(getActivity(), SettingsActivity.class));
+        return true;
+      }
+      return super.onOptionsItemSelected(item);
+    }
+  }
+
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  public static class LocationPreferenceFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      addPreferencesFromResource(R.xml.pref_location);
+      setHasOptionsMenu(true);
+
       bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
     }
 
